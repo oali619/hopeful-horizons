b3d8cd3e69164805254466a51cf8cb18e739cc71
oali619 <obsaahmedali@gmail.com>
2025-07-31 11:57:19 -0500

    add metatags

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
 import './root.css';
 import '../app/components/index.css';
 import { Toaster } from 'sonner';
+import img from './images/autism0.jpg';
 
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
@@ -17,6 +18,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
 				<meta name='keywords' content='Autism, ABA, Minnesota, Twin-Cities, Support,Therapy, Resources, Children, Social work' />
 				<meta name='description' content='Compassionate autism support, ABA therapy, resources, and services for individuals and families in Minnesota.' />
 				<meta name='robots' />
+				<meta property='og:title' content='Hopeful Horisons ABA Therapy MN' />
+				<meta property='og:description' content='Compassionate autism support, ABA therapy, resources, and services for individuals and families in Minnesota.' />
+				<meta property='og:image' content={img} />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<Meta />
 				<Links />
